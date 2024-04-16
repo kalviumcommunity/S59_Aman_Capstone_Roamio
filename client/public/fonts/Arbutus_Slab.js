5152d826319fc6a491d13a1d4b259f3c58692bc3
hiamanagrawal <hiamanagrawal@gmail.com>
2024-04-16 17:22:57 +0530

    added and designed navbar

---
+++ client/public/fonts/Arbutus_Slab.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Arbutus_Slab } from '@next/font/google'
+
+
+const ArbutusSlab = Arbutus_Slab({
+  subsets: ['latin'],
+  weight: ['400']
+})
+
+export default ArbutusSlab
+
+
+
+
