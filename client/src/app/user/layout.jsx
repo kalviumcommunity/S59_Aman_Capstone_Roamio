d7249af31bd48735d07f8c201d5c37056b7d3b8b
hiamanagrawal <hiamanagrawal@gmail.com>
2024-05-09 01:36:56 +0530

    added main navigation

---
+++ client/src/app/user/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideNavigation from "../components/SideNavigation/SideNavigation";
+
+function layout() {
+  return (
+    <div>
+    <SideNavigation />
+    </div>
+  )
+}
+
+export default layout
