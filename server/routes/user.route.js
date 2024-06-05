f41965e7cea844ca31e10f82f6a66488a7701698
hiamanagrawal <hiamanagrawal@gmail.com>
2024-06-05 16:21:15 +0530

    implemented suggestions by codum ai for jwt token

---
+++ server/routes/user.route.js
@@ -0,0 +1,38 @@
+import express from "express";
+import {
+  uploadFileTypeValidate,
+  validateFileSize,
+} from "../middlewares/UploadFileValidate.js";
+import uploadFileToFirebase from "../middlewares/uploadFileToFirebase.js";
+import {
+  addUser,
+  doesUserExist,
+  loginUser,
+  userPublicDetails,
+} from "../controllers/user.controller.js";
+import { rateLimit } from "express-rate-limit";
+
+const loginLimiter = rateLimit({
+  max: 5,
+  message: "too many login attempts from this IP  , Please try again later",
+});
+
+const userRoutes = express.Router();
+
+userRoutes.get("/", userPublicDetails);
+
+userRoutes.post(
+  "/add-user",
+  uploadFileTypeValidate,
+  validateFileSize,
+  (req, res, next) => {
+    uploadFileToFirebase("profile", req, res, next);
+  },
+  addUser
+);
+
+userRoutes.get("/doesUserExist", doesUserExist);
+
+userRoutes.post("/login", loginLimiter, loginUser);
+
+export default userRoutes;
